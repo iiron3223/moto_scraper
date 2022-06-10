299ed9d9d868588b7453fef0bbd76935ac6fc96e
Dominik Baran <iiron3223@gmail.com>
2022-06-10 23:42:10 +0200

    feat: Create basic MotoSpider

--- otomoto/otomoto_scraper/spiders/moto_spider.py
+++ otomoto/otomoto_scraper/spiders/moto_spider.py
@@ -1,10 +1,30 @@
 import scrapy
+from scrapy.loader import ItemLoader
 
 
-class MotoSpiderSpider(scrapy.Spider):
+class MotoSpider(scrapy.Spider):
     name = "moto_spider"
     allowed_domains = ["otomoto.pl"]
-    start_urls = ["http://www.otomoto.pl/"]
+    start_urls = [
+        "https://www.otomoto.pl/osobowe/seg-cabrio--seg-city-car--seg-combi--seg-compact--seg-coupe--seg-mini--seg-minivan--seg-suv/kielce?search%5Bfilter_enum_fuel_type%5D=petrol&search%5Bdist%5D=25&search%5Bfilter_float_mileage%3Ato%5D=250000&search%5Bfilter_float_price%3Afrom%5D=10000&search%5Bfilter_float_price%3Ato%5D=20000&search%5Badvanced_search_expanded%5D=true"
+    ]
 
     def parse(self, response):
-        pass
+        articles = response.css("article")
+
+        for article in articles:
+            # fmt: off
+            info = article.css('ul').css('li::text').getall()[:4]
+            yield{
+
+                  'name': article.css('a::text').get(),
+                  'id': article.attrib['id'],
+                  'url': article.css('a').attrib['href'],
+                  'price': article.css('span::text').get(),
+                  'year': info[0],
+                  'distance': info[1],
+                  'engine_volume': info[2],
+                  'fuel': info[3],
+                  'photo_url': article.css('img').attrib['src'],
+            }
+            # fmt: on
